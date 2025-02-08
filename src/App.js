b45f11e3035b47c50d41cbcf2038df8465c42991
nishthasingh20 <singhnishtha206@gmail.com>
2025-02-08 11:43:14 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './App.css';
+import logo from '../src/logo.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-sm-2'>
+            <img src={logo} />
+          </div>
+        </div>
+      </div>
+    </header>
+    </>
   );
 }
 
